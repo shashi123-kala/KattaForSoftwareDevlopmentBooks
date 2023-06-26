bd990d5cd97951ee1cde8a62583147fa8338f770
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-26 07:14:35 +0530

    Add:Buying three different books

--- src/main/java/com/sdb/tdd/CalculateDiscount.java
+++ src/main/java/com/sdb/tdd/CalculateDiscount.java
@@ -6,7 +6,10 @@ public class CalculateDiscount {
 		if (noOfBooks == 1) {
 			amtAfterDis = 50 - (50 * (dis / 100));
 		} else if (noOfBooks == 2) {
-			double count = noOfBooks * 50.00;
+			double count = 2 * 50.00;
+			amtAfterDis = count - (count * (dis / 100));
+		} else if (noOfBooks == 3) {
+			double count = 3 * 50.00;
 			amtAfterDis = count - (count * (dis / 100));
 		}
 		return amtAfterDis;
